2e54682a1e418f28fac36d59309690f3b29046f2
Daniel <dan.wojarski@gmail.com>
2020-06-08 14:30:28 +0200

    finished tabbed page

--- ResQuod/ResQuod/ResQuod.Android/Resources/Resource.designer.cs
+++ ResQuod/ResQuod/ResQuod.Android/Resources/Resource.designer.cs
@@ -5798,10 +5798,25 @@ namespace ResQuod.Droid
 			public const int Scanning_wait = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int tooltip_frame_dark = 2131165303;
+			public const int tabbed_addchip = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int tooltip_frame_light = 2131165304;
+			public const int tabbed_attendance = 2131165304;
+			
+			// aapt resource value: 0x7F070079
+			public const int tabbed_events = 2131165305;
+			
+			// aapt resource value: 0x7F07007A
+			public const int tabbed_home = 2131165306;
+			
+			// aapt resource value: 0x7F07007B
+			public const int tabbed_user = 2131165307;
+			
+			// aapt resource value: 0x7F07007C
+			public const int tooltip_frame_dark = 2131165308;
+			
+			// aapt resource value: 0x7F07007D
+			public const int tooltip_frame_light = 2131165309;
 			
 			static Drawable()
 			{
