d194d58bfede8046efc15359f40ff099a907217b
Daniel <dan.wojarski@gmail.com>
2020-06-21 14:33:29 +0200

    api connection for events

---
+++ ResQuod/ResQuod/ResQuod/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResQuod.Models
+{
+    class Room
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public Corpo Corpo { get; set; }
+    }
+}
--- ResQuod/ResQuod/ResQuod/Views/MainViews/EventsPanel.xaml.cs
+++ ResQuod/ResQuod/ResQuod/Views/MainViews/EventsPanel.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using ResQuod.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using ResQuod.Views.MainViews.EventPanelViews;
 
 namespace ResQuod.Views.MainViews
 {
@@ -14,11 +16,24 @@ namespace ResQuod.Views.MainViews
         public EventsPanel()
         {
             InitializeComponent();
+            BindingContext = new ExplorePage();
         }
 
         public void onNavigated()
         {
 
+        }
+
+        private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            var selected_event = args.CurrentSelection.FirstOrDefault() as Event;
+            if (selected_event == null) return;
+
+            //TODO
+            //[before burdel] async nawigacja do strony z wpisaniem kodu dostepu do wydarzenia
+            //[after burdel] emm no po kliknieciu w wydarzenie to chyba juz do strony z nfc tagiem powinno przenosic tez z async
+
+            ((CollectionView)sender).SelectedItem = null;
         }
     }
 }
