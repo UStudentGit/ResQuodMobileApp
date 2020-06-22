cd9068200fc8bc4eae6b363b5a7b4b95b8d0f923
Daniel <dan.wojarski@gmail.com>
2020-06-22 14:05:23 +0200

    removed useless files

--- ResQuod/ResQuod/ResQuod/Views/MainViews/EventsPanel.xaml.cs
+++ ResQuod/ResQuod/ResQuod/Views/MainViews/EventsPanel.xaml.cs
@@ -7,7 +7,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
-using ResQuod.Views.MainViews.EventPanelViews;
 
 namespace ResQuod.Views.MainViews
 {
@@ -16,24 +15,11 @@ namespace ResQuod.Views.MainViews
         public EventsPanel()
         {
             InitializeComponent();
-            BindingContext = new ExplorePage();
         }
 
         public void OnNavigated()
         {
 
-        }
-
-        private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs args)
-        {
-            var selected_event = args.CurrentSelection.FirstOrDefault() as Event;
-            if (selected_event == null) return;
-
-            //TODO
-            //[before burdel] async nawigacja do strony z wpisaniem kodu dostepu do wydarzenia
-            //[after burdel] emm no po kliknieciu w wydarzenie to chyba juz do strony z nfc tagiem powinno przenosic tez z async
-
-            ((CollectionView)sender).SelectedItem = null;
         }
     }
 }
